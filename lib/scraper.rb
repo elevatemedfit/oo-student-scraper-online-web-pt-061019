bfa0ba2c7bb98befe4ed6d665215e5f9f6c1be3e
Chris Junker <elevatemedfit@gmail.com>
2019-07-29 12:17:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,24 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
+ index_url = "./fixtures/student-site/index.html"
 
+  def self.scrape_index_page(index_url)
+  index_url = open('http://web0415.students.flatironschool.com/')
+  profile_data = Nokogiri::HTML(index_url)
+  students = {}
+
+  profile_data.css("div.big-comment h3 a").each_with_index do |student,i|
+
+    # ...organize data into respective key-value pairs...
+
+  end
+  students
+end
+
+  
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
